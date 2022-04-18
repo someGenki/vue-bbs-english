1f1c241777a306a2f774f60ac78beeb938392b4f
hejiyuan <1159140147@qq.com>
2022-04-18 11:21:18 +0800

    feat: ReadCard.vue实现

--- src/views/read/ReadCard.vue
+++ src/views/read/ReadCard.vue
@@ -1,11 +1,119 @@
 <template>
-  <div></div>
+  <router-link :to="`/article/${data.id}`" class="read-card">
+    <img class="first-pic" :src="data.firstPic" alt="" />
+    <div class="card-introduce">
+      <h3 class="title">{{ data.title }}</h3>
+      <div class="description">{{ data.description }}</div>
+      <div class="infos">
+        <span class="difficulty" :style="{ color }">
+          {{ difficultyText }}
+        </span>
+        <span class="time">{{ data.gmtCreate }}</span>
+        <span class="view"><i />{{ data.pv }}</span>
+      </div>
+    </div>
+  </router-link>
 </template>
 
 <script setup>
-defineProps({
+import { computed } from 'vue'
+
+const props = defineProps({
+  // [id, firstPic, title, description, difficulty, pv, gmtCreate]
   data: { type: Object, required: true },
+})
+
+const color = computed(() => {
+  switch (props.data.difficulty) {
+    case 1:
+      return '#06D6A0'
+    case 2:
+      return '#FFD166'
+    case 3:
+      return '#FF6F59'
+    case 4:
+      return '#26547C'
+  }
+})
+
+const difficultyText = computed(() => {
+  switch (props.data.difficulty) {
+    case 1:
+      return '小学水平'
+    case 2:
+      return '中学水平'
+    case 3:
+      return '大学水平'
+    case 4:
+      return '商务水平'
+  }
 })
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.read-card {
+  display: block;
+  color: #6d757d;
+  height: 170px;
+  cursor: pointer;
+  border-radius: 4px;
+  padding: 10px;
+  background-color: #ffffff;
+  transition: transform 200ms;
+
+  &:hover {
+    transform: translateY(-3px);
+    box-shadow: 12px 12px 20px rgba(0, 0, 0, 0.05);
+  }
+}
+
+.first-pic {
+  float: left;
+  margin-right: 12px;
+  width: 200px;
+  height: 150px;
+  border-radius: 6px;
+}
+
+.card-introduce {
+  display: flex;
+  flex-direction: column;
+  padding-left: 6px;
+  height: 145px;
+
+  & > .title {
+    color: #222222;
+    margin-top: 2px;
+  }
+
+  & > .description {
+    color: #6d757d;
+    flex: 1;
+  }
+
+  & > .infos {
+    & > * {
+      margin-right: 12px;
+    }
+
+    .difficulty {
+      font-weight: bold;
+    }
+
+    .view {
+      display: inline-flex;
+      align-items: center;
+      float: right;
+      margin-right: 28px;
+
+      & > i {
+        height: 14px;
+        width: 14px;
+        margin-right: 4px;
+        background-size: 100%;
+        background-image: url('/src/icons/view.png');
+      }
+    }
+  }
+}
+</style>
--- src/views/read/index.vue
+++ src/views/read/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="box">
     <read-card :data="data" />
   </div>
 </template>
@@ -8,12 +8,12 @@
 import ReadCard from './ReadCard.vue'
 
 const data = {
-  collections: 0,
   content:
     "![313.jpg](http://fanyibar.top:8086/archive/jpg/1601539949183313.jpg) \r Qingdao aims to become a hotbed for innovation. [Photo/WeChat account: qdfbwx]\r \r The Chinese enterprise reform 50 forum and the Conference on Building a Creative Qingdao (CBCQ) was held in Qingdao, East China's Shandong province on Sept 25.\r \r This is the first time that the reform forum has been held in the coastal city since it was founded by the China Enterprise Reform and Development Society in Beijing on Dec 16, 2019.\r \r The forum, with a focus on \"deepening reform to release new development momentum and fostering a new economic growth pattern of 'dual circulation'\", took place both online and offline.\r During the event, government officials, renowned experts and industrial leaders brainstormed key issues in the process of reforming enterprises, including upgrading the business  environment to promote reforms to enterprise mechanisms, new stimulation for consumption, and new development models in the post-pandemic era.\r Combining with CBCQ's slogan of \"open, modern, dynamic, stylish\", the event was meant to boost the innovative development of Qingdao through the collective wisdom of governments, businesses and society at large.\r \r Entrepreneurs, scholars, sister cities and overseas partners, anyone with an innovative idea is welcome in Qingdao, as only creative thinking can create a bright future for the city, said local media.\r \r Since the last CBCQ was held on June 28, 2019, the city has witnessed significant changes, with surging growth in fixed asset investment, China's first artificial intelligence industry community established in the city, and unprecedented incentives carried out to promote innovation among private enterprises.\r High-end industries such as industrial internet, new energy vehicles, 5G and biomedicine are coming together in Qingdao. The city is creating an open platform for innovative resources from around the world.\r \r Source：CHINA DAILY-Charming Qingdao\r read more\r 原标题：《英语新闻｜Reform forum helps build a creative Qingdao》",
-  description: '中国企业改革50人论坛暨创意青岛大会召开',
-  difficulty: 3,
+  description: '中国企业改革50人论坛暨创意青岛大会召开NEW',
   firstPic: 'http://res7.fanyibar.top/1601539421210-313.jpg',
+  collections: 0,
+  difficulty: 3,
   gmtCreate: '2020-10-01 16:03:47',
   gmtModified: '2022-04-13 18:50:27',
   id: 2,
@@ -26,4 +26,12 @@ const data = {
 }
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.box{
+  position: relative;
+  width: 100%;
+  max-width: 1200px;
+  margin: 0 auto;
+}
+
+</style>
