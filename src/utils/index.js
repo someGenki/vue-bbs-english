9d697d6584a717ace9903b634eecc18c54f35905
hejiyuan <1159140147@qq.com>
2022-04-20 17:44:06 +0800

    perf: 对库文件进行抽离，减少vendor的体积，也更方便缓存。合并小文件，减少请求次数

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,19 +1,3 @@
-// from Vue-element-admin @PanJiaChen
-export function hasClass(ele, cls) {
-  return !!ele.className.match(new RegExp('(\\s|^)' + cls + '(\\s|$)'))
-}
-
-export function addClass(ele, cls) {
-  if (!hasClass(ele, cls)) ele.className += ' ' + cls
-}
-
-export function removeClass(ele, cls) {
-  if (hasClass(ele, cls)) {
-    const reg = new RegExp('(\\s|^)' + cls + '(\\s|$)')
-    ele.className = ele.className.replace(reg, ' ')
-  }
-}
-
 /**
  * 将中划线格式字符串驼峰化
  * @example first-name => firstName
