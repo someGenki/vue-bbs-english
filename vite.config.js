83185a2364214c123e77cf7996e1786fa24291f1
hejiyuan <1159140147@qq.com>
2022-04-20 17:44:06 +0800

    perf: 对库文件进行抽离，减少vendor的体积，也更方便缓存。合并小文件，减少请求次数

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,32 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
+const libNameReg = /\/node_modules\/([^/]+)\//
+const commonReg = /\/src\/(utils|api|hooks)\//
+const extractLibs = [
+  '@vue',
+  '@ctrl',
+  'vue-router',
+  'element-plus',
+  '@element-plus',
+  'axios',
+  'marked',
+  '@popperjs',
+  'lodash-es',
+  'async-validator',
+]
+// 对库文件进行抽离，减少vendor的体积，也更方便缓存。合并小文件，减少请求次数
+const manualChunks = (id) => {
+  if (libNameReg.test(id)) {
+    const libName = RegExp.$1
+    return extractLibs.includes(libName) ? libName : 'vendor'
+  } else if (commonReg.test(id)) {
+    return 'common' // 合并零散的通用文件
+  } else if (id.includes('css')) {
+    return 'styles' // 合并零散的样式文件
+  }
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/bbs/', // 可被命令行参数 --base=/xxx/ 覆盖
@@ -11,6 +37,8 @@ export default defineConfig({
   },
   build: {
     reportCompressedSize: false,
+    target: 'es2020',
+    rollupOptions: { output: { manualChunks } },
   },
   plugins: [vue(), vueJsx()],
   resolve: {
