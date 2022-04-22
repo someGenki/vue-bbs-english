7ded7a380f3503dea420bfeeae3f497b408cf3f4
hejiyuan <1159140147@qq.com>
2022-04-22 10:47:57 +0800

    feat: 用户空间顶部板块搭建

---
+++ src/views/space/SpaceHeader.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="user-space-header">
+    <!-- 背景图 -->
+    <div class="user-cover">
+      <img :src="userinfo.coverImg" v-default-img="'background'" />
+    </div>
+    <div class="user-info">
+      <img
+        class="user-avatar"
+        :src="userinfo.avatar"
+        v-default-img="userinfo.nickname"
+      />
+      <div class="detail">
+        <div>
+          <span class="user-nickname">
+            {{ userinfo.nickname }}
+          </span>
+          <span class="sex" v-if="userinfo.sex < 3">
+            <app-icon size="20" :icon="iconName" :color="iconColor" />
+          </span>
+          <span class="point">{{ null }}</span>
+        </div>
+        <div class="user-sign">
+          <app-icon icon="el-icon-mic" />
+          {{ userinfo.sign }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const { userinfo } = defineProps({
+  userinfo: { type: Object, required: true },
+})
+const iconName = `el-icon-${userinfo.value === 1 ? 'male' : 'female'}`
+const iconColor = userinfo.value === 1 ? '#40a9ff' : '#ff7474'
+</script>
+
+<style lang="scss" scoped>
+.user-space-header {
+  overflow: hidden;
+  height: 340px;
+  background: #ffffff;
+
+  & > .user-cover {
+    position: relative;
+    max-height: 250px;
+    box-shadow: 0 2px 6px rgba(0, 0, 0, 0.15);
+
+    & > img {
+      max-height: 250px;
+      width: 100%;
+    }
+  }
+
+  & > .user-info {
+    position: relative;
+
+    > .user-avatar {
+      border: 3px solid #ebe0e0;
+      position: absolute;
+      border-radius: 8px;
+      height: 100px;
+      left: 10px;
+      top: -24px;
+    }
+
+    > .detail {
+      position: absolute;
+      left: 125px;
+      top: 5px;
+
+      .user-nickname {
+        margin-right: 12px;
+        font-size: 26px;
+        font-weight: bold;
+      }
+
+      .user-sign {
+        display: flex;
+        align-items: center;
+        margin-top: 12px;
+      }
+    }
+  }
+}
+</style>
--- src/views/space/index.vue
+++ src/views/space/index.vue
@@ -1,11 +1,34 @@
 <template>
-<div>space</div>
+  <div v-if="loading" class="user-space">
+    <space-header :userinfo="userinfo" />
+    div.
+  </div>
 </template>
 
 <script setup>
+import { ref } from 'vue'
+import { useRoute } from 'vue-router'
+import { getUserInfoDigest } from '/src/api/user'
+import SpaceHeader from './SpaceHeader.vue'
 
+const route = useRoute()
+const uid = route.params.uid
+const loading = ref(false)
+const userinfo = ref(null)
+
+getUserInfoDigest(uid).then((res) => {
+  loading.value = true
+  userinfo.value = res.data
+})
 </script>
 
 <style lang="scss" scoped>
+.user-space {
+  max-width: 1000px;
+  margin: -8px auto 8px;
+  min-height: 800px;
 
+  border-radius: 2px;
+  overflow: hidden;
+}
 </style>
